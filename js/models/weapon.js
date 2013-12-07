30a8448cc52416f49f55cb4b77e6886866dfb0d0
aplopp <anthony@anthonyplopper.com>
2013-12-06 18:03:27 -0600

    Added buggy but somewhat suitable code for sending a projectile in the right direction.

--- js/models/weapon.js
+++ js/models/weapon.js
@@ -3,8 +3,9 @@ define([
 	'backbone',
 	'createjs', 
 	'settings',
-	'projectile'
-], function( _, Backbone, createjs, settings, Projectile ){
+	'projectile', 
+	'functions/math',
+], function( _, Backbone, createjs, settings, Projectile, mathFunctions ){
 	/**
 	 * maintains a model of the properties affecting the drawing of the object.
 	 * the view listens to these changes and adjusts the rendering accordingly
@@ -18,21 +19,33 @@ define([
 			speed: 500, // pixels/s
 			projectile: 'bullet',
 			auto: false,
-
-			holdingPlayer: false
+			holdingPlayer: false,
+			color: '#fff',
+			length: 10,
+			width: 2
 		},
 		launchProjectile: function(){
 			var player = app.players[ this.get( 'holdingPlayer' ) ] ;
+
+			var xDir = player.model.get( 'facing' ) === 'left' ? -1 : 1;
+			var yDir = player.model.get( 'aim' ) < 135 ? -1 : 1;
+			var barrelCoords = mathFunctions.getPointOnCircle( player.model.get( 'width' )/2, player.model.get( 'height' )/2, this.get('length'), xDir * player.model.get( 'aim' ) );
+
+			// TODO: accurate splitting of the speed into vX and vY
+			var aim = player.model.get( 'aim' );
+			console.log( aim%90/90 );
+			var percentvX = aim % 90/90 ;
+			var vX = xDir * this.get( 'speed' ) * percentvX;
+			var vY = yDir * this.get( 'speed' ) * ( 1 - percentvX );
 			var projectile = new Projectile({
-				// end of barrel coordinates
-				x: player.x, // TODO, end of barrel
-				y: player.y, // TODO, end of barrel
+				// e nd of barrel coordinates
+				x: player.x + barrelCoords.x, // TODO, end of barrel
+				y: player.y + barrelCoords.y, // TODO, end of barrel
 				// x/y components of speed, depending on angle of barrel
-				vX: this.get( 'speed' ) + player.vX,
-				vY: player.vY,
+				vX: vX + player.vX,
+				vY: vY + player.vY,
 				model: settings.projectiles[ this.get('projectile') ]
 			});
-			console.log( projectile ); 
 			app.addObject( projectile );
 		},
 		_delay: false,
